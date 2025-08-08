8638099fcdb0c16ce31facba74bb9b287c5a7ff6
Abdullah <itsabdullah80@gmail.com>
2025-08-08 21:55:05 +0300

    batman

---
+++ src/main/java/com/abdul/temporaltesting/TemporalTestingApplication.java
@@ -0,0 +1,13 @@
+package com.abdul.temporaltesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdul/temporaltesting/TemporalTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdul.temporaltesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemporalTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
